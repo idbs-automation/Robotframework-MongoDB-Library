8abadc8561a84a64af93b553bdb86ed458c7e0af
stevejefferiesIDBS <sjefferies@idbs.com>
2015-05-05 11:29:00 +0100

    Use setuptools over distutils
    Use setuptools to allow external dependencies to be included

--- setup.py
+++ setup.py
@@ -3,25 +3,29 @@
 
 """Setup script for Robot's MongoDB Library distributions"""
 
-from distutils.core import setup
+from setuptools import setup
 
-import sys, os
-sys.path.insert(0, os.path.join('src','MongoDBLibrary'))
+import sys
+import os
+
+sys.path.insert(0, os.path.join('src', 'MongoDBLibrary'))
 
 from version import VERSION
 
-def main():
-    setup(name         = 'robotframework-mongodblibrary',
-          version      = VERSION,
-          description  = 'Mongo Database utility library for Robot Framework',
-          author       = 'Jerry Schneider',
-          author_email = 'jerry@iplantcollaborative.org',
-          url          = 'https://github.com/',
-          package_dir  = { '' : 'src'},
-          packages     = ['MongoDBLibrary'],
+
+def main(scriptargs):
+    setup(name='robotframework-mongodblibrary',
+          version=VERSION,
+          description='Mongo Database utility library for Robot Framework',
+          author='Jerry Schneider',
+          author_email='jerry@iplantcollaborative.org',
+          url= 'https://github.com/',
+          package_dir={'': 'src'},
+          packages=['MongoDBLibrary'],
           install_requires=['robotframework', 'pymongo>=3.0.0'],
+          script_args=scriptargs
           )
         
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
